73ba676c8c831fc897fb8ed95bfdf37b2989177a
Your Name <youremail@yourdomain.com>
2023-05-04 15:17:14 +0700

    change

--- excell_on/webapi/Models/ExcellOnDbContext.cs
+++ excell_on/webapi/Models/ExcellOnDbContext.cs
@@ -35,7 +35,7 @@ namespace webapi.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server= .\\ELIZABETH; Initial Catalog= Excell-On-Db; User ID = sa; Password = 1 ; Integrated Security=True; Trusted_Connection=true ; TrustServerCertificate=True");
+                optionsBuilder.UseSqlServer("Server=tcp:mysqlserver2409.database.windows.net,1433; Database=Excell-On-Db;Persist Security Info=False;User ID=azureAdmin;Password=Group2project3;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }
 =======
