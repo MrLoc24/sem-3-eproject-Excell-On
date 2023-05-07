d2861b1f53d3d17786f21c6fd60f13b412397035
Your Name <youremail@yourdomain.com>
2023-05-07 19:52:40 +0700

    change customer header

---
+++ excell_on/reactapp/src/views/serviceCustomer/ServiceCustomer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Breadcrumb } from "src/components";
+
+export default function ServiceCustomer() {
+  return (
+    <>
+        <Breadcrumb title="In-bound Service"/>
+        
+
+    </>
+  )
+}
